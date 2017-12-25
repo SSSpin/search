ed52ecb1514298b6c9f4fd80acfae26cd42411ff
lijinlong <lijinlong@kingxique.com>
2017-12-26 01:13:25 +0800

    更新发布

--- web/config/dev.env.js
+++ web/config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"development"',
     ENV_CONFIG: '"dev"',
-    BASE_API: '"http://0.0.0.0:3000"',
+    BASE_API: '"http://118.31.1.173:3000"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 }
